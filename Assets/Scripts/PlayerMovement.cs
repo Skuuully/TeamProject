c3566d96cd3039ef0187f5075c1005b5e8db4e88
Ryan Skull <skuuully@gmail.com>
2019-01-31 14:11:01 +0000

    Added left and right player movement as well as a terrible jump.

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public float playerSpeed;
+    public float jumpHeight;
+    float horizontalMove;
+    float verticalMove;
+    bool jump;
+    Vector2 velocity;
+    public Rigidbody2D rb2d;
+
+    // Use this for initialization
+    void Start()
+    {
+        horizontalMove = 0;
+        verticalMove = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!Input.GetButton("Jump"))
+        {
+            verticalMove = 0;
+        }
+        else
+        {
+            verticalMove = jumpHeight;
+        }
+        horizontalMove = Input.GetAxis("Horizontal");
+    }
+
+    void FixedUpdate()
+    {
+        velocity.Set(horizontalMove * playerSpeed * Time.deltaTime, verticalMove * Time.deltaTime);
+        rb2d.velocity = (velocity);
+    }
+}
