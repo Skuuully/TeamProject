c0302492500d7c2d5b4abb999784ac743e25b1b6
Ryan Skull <skuuully@gmail.com>
2019-02-13 09:08:34 +0000

    Started adding a main menu, buttons currently change color when hovered and the levels button takes you to a new empty scene where a level select can be setup

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+    public bool isQuit;
+    public bool isLevels;
+    public bool isInfinite;
+    public bool isSettings;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnMouseUp()
+    {
+        if(isLevels)
+        {
+            SceneManager.LoadScene("Levels");
+        }
+        else if(isQuit)
+        {
+            Application.Quit();
+        }
+        else if(isSettings)
+        {
+
+        }
+        else if(isInfinite)
+        {
+
+        }
+    }
+}
---
+++ Assets/Scripts/MouseHover.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseHover : MonoBehaviour {
+    // Cannot figure out why this public color system always sets it transparent 
+    // using hard coded for now
+    public Color32 defaultColor;
+    public Color32 hoverColor;
+    
+	// Use this for initialization
+	void Start () {
+        defaultColor = Color.white;
+        hoverColor = Color.red;
+        GetComponent<Renderer>().material.color = defaultColor;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnMouseEnter()
+    {
+        GetComponent<Renderer>().material.color = hoverColor;
+    }
+
+    void OnMouseExit()
+    {
+        GetComponent<Renderer>().material.color = defaultColor;
+    }
+}
