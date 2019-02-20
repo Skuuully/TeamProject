3ca7ab3f2ed59626e28c2bc0e37a946a0ccde9bc
Ryan Skull <skuuully@gmail.com>
2019-02-20 12:06:59 +0000

    Started setup for infinite scene, also possibly fixed slowly running away from the wall

---
+++ Assets/Scripts/spawner.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawner : MonoBehaviour
+{
+    // how often to spawn block or damager
+    public float delay;
+    private float time;
+
+    public Transform leftWall;
+    public Transform rightWall;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time += Time.deltaTime;
+        if(time > delay)
+        {
+            time = 0;
+            Spawn();
+        }
+    }
+
+    void Spawn()
+    {
+        if(gameObject.name == "left spawner")
+        {
+            Instantiate(leftWall);
+        }
+        
+    }
+}
